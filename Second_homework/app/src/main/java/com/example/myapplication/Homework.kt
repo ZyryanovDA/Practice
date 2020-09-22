c39a78f43cf529be12b206d61091088e5f233adc
Danila Zyryanov <zyryanow.d.a@gmail.com>
2020-09-22 09:42:55 +0500

    resolved second homework

--- Second_homework/app/src/main/java/com/example/myapplication/Homework.kt
+++ Second_homework/app/src/main/java/com/example/myapplication/Homework.kt
@@ -2,15 +2,20 @@ package com.example.myapplication
 import kotlin.math.*
 
 fun main() {
-    var Resolve = solveEquation(3, 4, 2)
+    var Resolve = solveEquation(2, 3, 1)
     print(Resolve)
 }
 
 fun solveEquation(a: Int, b: Int, c: Int): Double {
     var b = b.toDouble()
     val D = b.pow(2.0) - (4 * a * c)
-    var x1: Double = (-b - sqrt(D))/(2 * a)
-    var x2: Double = (-b + sqrt(D))/(2 * a)
-
+    var x1: Double = 0.0
+    var x2: Double = 0.0
+    if (D > 0) {
+        x1 = (-b - sqrt(D))/(2 * a)
+        x2 = (-b + sqrt(D))/(2 * a)
+    } else {
+        println("D <= 0")
+    }
     return(x1 + x2)
 }
