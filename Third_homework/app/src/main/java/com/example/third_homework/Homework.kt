282001c7893ddd8781582bdd7dea34e8bfbc00f6
Danila Zyryanov <zyryanow.d.a@gmail.com>
2020-09-24 16:56:20 +0500

    First half of work comleted

--- Third_homework/app/src/main/java/com/example/third_homework/Homework.kt
+++ Third_homework/app/src/main/java/com/example/third_homework/Homework.kt
@@ -1,5 +1,12 @@
 package com.example.third_homework
 
-fun main () {
-
+fun main() {
+    print("Введите число и нажмите кнопку Enter:")
+    var KeyboardNumber: Int = readLine()?.toIntOrNull() ?: return
+    println(KeyboardNumber)
+    println("Введите число и нажмите кнопку Enter, повторите 5 раз:")
+    val KeyRange = 1..5
+    for (i in KeyRange){
+        var i: Int = readLine()?.toIntOrNull() ?: return
+    }
 }
