cc6e473847bc7402bd40bf2699a4ac2e504c87cf
Danila Zyryanov <zyryanow.d.a@gmail.com>
2020-09-24 20:45:13 +0500

    Mistakes and troubles with breaks in 3-homework

--- Third_homework/app/src/main/java/com/example/third_homework/Homework.kt
+++ Third_homework/app/src/main/java/com/example/third_homework/Homework.kt
@@ -5,8 +5,12 @@ fun main() {
     var KeyboardNumber: Int = readLine()?.toIntOrNull() ?: return
     println(KeyboardNumber)
     println("Введите число и нажмите кнопку Enter, повторите 5 раз:")
+    //переделай под функцию Cicles
+}
+fun Cicles(): Int {
     val KeyRange = 1..5
     for (i in KeyRange){
         var i: Int = readLine()?.toIntOrNull() ?: return
+        if (i !is Int) continue
     }
 }
